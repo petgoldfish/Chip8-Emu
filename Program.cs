5479f4fdec5487948087ad631afa8c0bb872204c
petgoldfish <grs348@nyu.edu>
2020-01-04 20:12:56 -0500

    Read ROMs and dump hex

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace Chip8Emulator
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			using (BinaryReader reader = new BinaryReader(new FileStream("roms\\test.ch8", FileMode.Open)))
+			{
+				while (reader.BaseStream.Position < reader.BaseStream.Length)
+				{
+					ushort opcode = (ushort)((reader.ReadByte() << 8) | reader.ReadByte());
+					Console.WriteLine($"{opcode.ToString("X4")}");
+				}
+
+				Console.ReadKey();
+			}
+		}
+	}
+}
