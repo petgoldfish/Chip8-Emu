b0fa80b310107be57ba981920a100c8488c9b09a
petgoldfish <grs348@nyu.edu>
2020-01-18 18:14:19 -0500

    Add CPU and setup executeOpcode

--- Program.cs
+++ Program.cs
@@ -19,4 +19,36 @@ namespace Chip8Emulator
 			}
 		}
 	}
+
+	public class CPU
+	{
+		public byte[] Memory = new byte[4096];    // RAM
+		public byte[] V = new byte[16];           // Registers V0 to VF
+		public ushort I;                          // Address Register
+		public ushort PC;                         // Program Counter
+		public ushort[] Stack = new ushort[24];   // Stack
+		public byte SP = 0;                       // Stack Pointer 
+		public byte DelayTimer;                   // Delay Timer
+		public byte SoundTimer;                   // Sound Timer
+		public byte KeyBoard;                     // Keyboard Input
+		public byte[] Screen = new byte[64 * 32]; // Screen
+
+		public void executeOpcode(ushort opcode)
+		{
+			switch (opcode & 0xF000)
+			{
+				case 0x0000:
+					if (opcode == 0x00E0) // Clear Screen
+					{
+						for(int i = 0; i < Screen.Length; i++) Screen[i] = 0;
+					}
+					else if (opcode == 0x00EE) // Return from subroutine
+					{
+						PC = Stack[SP--];
+					}
+					break;
+					
+			}
+		}
+	}
 }
